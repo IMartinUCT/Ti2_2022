8ed890bd52a95a14e7ea0c2c2c8e8c9027046b62
patoskixd <patricio62014@gmail.com>
2022-09-20 21:08:12 -0300

    carpeta nueva
    nuevo

---
+++ integracion2/resources/views/header.blade.php
@@ -0,0 +1,28 @@
+<DOCTYPE html> 
+<html lang="en">
+    <head>
+    <title>Universidad Catolica De Temuco</title>
+    <link rel="shortcut icon" href="imagen/favicon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script src="{{ URL::asset('js/func.js') }}"></script>
+</head>
+
+    <header>
+    <a href="<?php echo url('/')?>" id='logo-uct'><img src="{{ asset('photos/logo-uct.png') }}"></a>
+    
+    <div class="container">
+        <button class='user-menu'>    
+            <img src="{{ URL::asset('photos/user.png') }}" class='user-avatar'/>            
+        </button>
+        <div class='box hide'>
+            <ul id='menu'>
+                <li><a href="<?php echo url('/login') ?>">Login</a></li>
+                <li><a href="<?php echo url('/register') ?>">Registro</a></li>
+                <li><a href="<?php echo url('/documents') ?>">Documentos</a></li>
+                <li><a href="<?php echo url('/history') ?>">Historial</a></li>
+            </ul>
+        </div>
+    </div>  
+    </header>
+</html>
