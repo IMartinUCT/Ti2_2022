3a98e3c3a3fe1bcf68094c4f80b88ecb07bc4d34
brazann <bzapata2002@gmail.com>
2022-09-22 02:00:04 -0300

    Reconstruccion Header
    new header

--- integracion2/resources/views/footer.blade.php
+++ integracion2/resources/views/footer.blade.php
@@ -1,7 +1,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 <div class="navbar fixed-bottom row">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-0 border-top">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <p class="col-md-4 mb-0 text-muted">&copy; 2022 Choripán con manjar Company, Inc</p>
 
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
@@ -9,11 +9,13 @@
     </a>
 
     <ul id="lala" class="nav col-md-4 justify-content-end">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Inicio</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Login</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Registro</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Documentos</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Historial</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
     </ul>
   </footer>
 </div>
+
+<!-- "navbar fixed-bottom row" -->
--- integracion2/resources/views/index.blade.php
+++ integracion2/resources/views/index.blade.php
@@ -14,6 +14,7 @@
 
     <body>
         @include('header')
+<<<<<<< Updated upstream
         <br>
         <br>
         <br>
@@ -70,6 +71,8 @@
         
         </tbody>
     </table>
+=======
+>>>>>>> Stashed changes
     </body>
     <footer>@include('footer')</footer>
 </html>
