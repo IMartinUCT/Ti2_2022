98ba27e2c899c709561caf3c4ac841fe7fc086d6
Nicolas V <nvergara2021@alu.uct.cl>
2022-10-10 23:30:36 -0300

    deleted useless files

--- integra2/public/js/func.js
+++
@@ -1,9 +0,0 @@
-function colorChange(par){
-    par.style.color='red';
-}
-$(function() {     
-    $('.user-avatar').on('click', function(e) {
-      e.preventDefault();
-      $('.box').toggleClass('hide');
-    });
-  });
